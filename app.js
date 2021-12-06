156fc0d136ca3e791cb297c44ef5435d43679681
ManishPoduval <manishpoduval@hotmail.com>
2021-12-06 12:34:42 +0530

    middleware setup

--- app.js
+++ app.js
@@ -14,6 +14,32 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+// ---------------------------------------------------
+//      EXPRESS-SESSION CONFIG
+// ---------------------------------------------------
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET, //DON'T FORGET TO ADD THIS IN YOUR .'.env' File
+  resave: false,
+  saveUninitialized: false, 
+  cookie: {
+    maxAge: 1000 * 24* 60 * 60 // your cookie will be cleared after these seconds
+  },
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/ReactTodos",
+    // Time to Live for sessions in DB. After that time it will delete it!
+    ttl: 24* 60 * 60 // your session will be cleared after these seconds
+  })
+}));
+
+// ---------------------------------------------------
+//      EXPRESS-SESSION CONFIG
+// ---------------------------------------------------
+
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
