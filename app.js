805868e17d4eff88f75af0c1b2554d7c8a63cc3a
ManishPoduval <manishpoduval@hotmail.com>
2021-12-07 11:58:39 +0530

    code

--- app.js
+++ app.js
@@ -48,6 +48,9 @@ app.use("/api", allRoutes);
 const todoRoutes = require('./routes/todo.routes');
 app.use('/api', todoRoutes);
 
+const authRoutes = require("./routes/auth.routes");
+app.use("/api", authRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
